67ee611a33b6cfe4e8bcd9f3efcde5e71c11d7fe
Kirsten Netter <kirsten.netter@gmx.de>
2022-05-11 15:45:28 +0200

    refreshing on Enter prevented; add items by Enter and clearing input field implemented

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-function newItem(){
 
-    
+
+function newItem(){
+ 
     // create list
     let li = $('<li></li>');
     // read user input
     let inputText = $('#input').val();
     // append input to list
     li.append(inputText);
+    $('#input').val("");
+    
 
     // display warning if user did not enter anything
     if (inputText === '') {
@@ -32,11 +35,22 @@ function newItem(){
     // function: remove item from list
     crossOutButton.on("click", deleteListItem);
     function deleteListItem(){
-        li.addClass("delete")
+        li.addClass("delete");
     }
 
     // Reorder items
     $('#list').sortable();
+
 }
+
+// also add item if user hits Enter instead of Add button
+document.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key == "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        newItem();
+    }
+}); 
 
  
